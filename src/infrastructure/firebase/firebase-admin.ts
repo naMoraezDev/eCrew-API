7d4d8df7fb365fcc03467e85b48bb517292aabcb
Gabriel Moraes <gabrielmoraes@lance.com.br>
2024-06-26 13:47:50 -0300

    chore: update service account data

--- src/infrastructure/firebase/firebase-admin.ts
+++ src/infrastructure/firebase/firebase-admin.ts
@@ -1,11 +1,12 @@
 import * as firebaseAdmin from "firebase-admin";
+import serviceAccount from "./service-account.json";
 
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert({
-      privateKey: process.env.PRIVATE_FIREBASE_KEY,
-      projectId: process.env.PRIVATE_FIREBASE_PROJECT_ID,
-      clientEmail: process.env.PRIVATE_FIREBASE_CLIENT_EMAIL,
+      projectId: serviceAccount.project_id,
+      privateKey: serviceAccount.private_key,
+      clientEmail: serviceAccount.client_email,
     }),
   });
 }
