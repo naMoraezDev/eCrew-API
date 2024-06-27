5726f351fdc3437c86da731872ac985f73aaec23
Gabriel Moraes <gabrielmoraes@lance.com.br>
2024-06-26 23:44:54 -0300

    fix: nodemailer configs

--- src/infrastructure/repositories/newsletter.repository.ts
+++ src/infrastructure/repositories/newsletter.repository.ts
@@ -20,8 +20,8 @@ export class NewsletterRepository implements NewsletterServiceProtocol {
     });
     const emailConfig = {
       to: email,
-      from: "ePosts <newsletter@eposts.com>",
       subject: "Newsletter ePosts",
+      from: "ePosts <newsletter@eposts.com>",
       html: "<p>Obrigado por se inscrever na nosso newsletter!</p>",
     };
     await smtp.sendMail(emailConfig).catch((err) => console.log(err));
