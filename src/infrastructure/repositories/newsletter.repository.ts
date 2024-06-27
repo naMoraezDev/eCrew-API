3715644d691f69ccb9723830f98783a3b4b31219
Gabriel Moraes <gabrielmoraes@lance.com.br>
2024-06-26 23:36:33 -0300

    fix: nodemailer config

--- src/infrastructure/repositories/newsletter.repository.ts
+++ src/infrastructure/repositories/newsletter.repository.ts
@@ -20,7 +20,7 @@ export class NewsletterRepository implements NewsletterServiceProtocol {
     });
     const emailConfig = {
       to: email,
-      from: "ePosts<newsletter>",
+      from: "ePosts",
       subject: "Newsletter ePosts - Obrigado por se inscrever!",
       html: "<p>Obrigado por se inscrever no nosso newsletter!</p>",
     };
